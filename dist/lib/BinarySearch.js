4fb8754d89950a0454aa8246cfd37d6ec3b25406
xiaomiwujiecao <1410200695@qq.com>
2018-11-30 17:41:40 +0800

    update BinarySearch class
    test with mocha

--- dist/lib/BinarySearch.js
+++ dist/lib/BinarySearch.js
@@ -23,6 +23,81 @@ var BinarySearch = /** @class */ (function () {
             return this.bsearchInternally(a, low, mid - 1, value);
         }
     };
+    BinarySearch.prototype.bsearchFirstElement = function (a, n, value) {
+        var low = 0;
+        var high = n - 1;
+        while (low <= high) {
+            var mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            }
+            else if (a[mid] < value) {
+                low = mid + 1;
+            }
+            else {
+                if ((mid == 0) || (a[mid - 1] != value)) {
+                    return mid;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+    };
+    BinarySearch.prototype.bsearchLastElement = function (a, n, value) {
+        var low = 0;
+        var high = n - 1;
+        while (low <= high) {
+            var mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            }
+            if (a[mid] < value) {
+                low = mid + 1;
+            }
+            if ((mid == n - 1) || (a[mid + 1] != value)) {
+                return mid;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+    };
+    BinarySearch.prototype.bsearchFirstGTE = function (a, n, value) {
+        var low = 0;
+        var high = n - 1;
+        while (low <= high) {
+            var mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                if ((mid == n - 1) || (a[mid + 1] != value)) {
+                    return mid;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    };
+    BinarySearch.prototype.nsearchLastLTE = function (a, n, value) {
+        var low = 0;
+        var high = n - 1;
+        while (low <= high) {
+            var mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid + 1;
+            }
+            else {
+                if ((mid == n - 1) || (a[mid + 1] != value)) {
+                    return mid;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    };
     return BinarySearch;
 }());
 exports.BinarySearch = BinarySearch;
--- src/lib/BinarySearch.ts
+++ src/lib/BinarySearch.ts
@@ -30,4 +30,86 @@ export class BinarySearch {
       return this.bsearchInternally(a, low, mid - 1, value)
     }
   }
+
+  public bsearchFirstElement(a: Array<number>, n: number, value: number): any {
+    let low = 0
+    let high = n - 1
+    while (low <= high) {
+      let mid = low + ((high - low) >> 1)
+      if (a[mid] > value) {
+        high = mid - 1
+
+      }
+      else if (a[mid] < value) {
+        low = mid + 1
+      }
+      else {
+        if ((mid == 0) || (a[mid - 1] != value)) {
+          return mid
+        }
+        else {
+          high = mid - 1
+        }
+      }
+    }
+
+  }
+
+  public bsearchLastElement(a: Array<number>, n: number, value: number): any {
+    let low = 0
+    let high = n - 1
+    while (low <= high) {
+      let mid = low + ((high - low) >> 1)
+      if (a[mid] > value) {
+        high = mid - 1
+
+      }
+      if (a[mid] < value) {
+        low = mid + 1
+      }
+      if ((mid == n - 1) || (a[mid + 1] != value)) {
+        return mid
+      }
+      else {
+        low = mid + 1
+      }
+
+    }
+  }
+
+  public bsearchFirstGTE(a: Array<number>, n: number, value: number): any {
+    let low = 0
+    let high = n - 1
+    while (low <= high) {
+      let mid = low + ((high - low) >> 1)
+      if (a[mid] > value) {
+        if ((mid == n - 1) || (a[mid + 1] != value)) {
+          return mid
+        }
+        else {
+          low = mid + 1
+        }
+      }
+    }
+    return -1
+  }
+
+  public nsearchLastLTE(a: Array<number>, n: number, value: number): any {
+    let low = 0
+    let high = n - 1
+    while (low <= high) {
+      let mid = low + ((high - low) >> 1)
+      if (a[mid] > value) {
+        high = mid + 1
+      } else {
+        if ((mid == n - 1) || (a[mid + 1] != value)) {
+          return mid
+        }
+        else {
+          low = mid + 1
+        }
+      }
+    }
+    return -1
+  }
 }
